97d6add8a87d55c9afadb4d15eb8975f36555e00
Matthew Iadanza <fbsmi@fbsabsl-wkstn06.leeds.ac.uk>
2019-07-03 10:55:41 +0100

    now generates a star file

--- particle_DF_analysis.py
+++ particle_DF_analysis.py
@@ -29,49 +29,45 @@ def read_starfile_new(f):
 
 try:
     statedres = float(sys.argv[2])
+    cutoff = sys.argv[3]
+    pixelsize = 1.065
 except:
-    sys.exit('USAGE particle_DF_analysis.py <run_data.star> <stated resolution>')
+    sys.exit('USAGE particle_DF_analysis.py <run_data.star> <stated resolution> <cutoff 0.05 or 0.01>')
+if cutoff not in ['0.05','0.01']:
+    sys.exit('USAGE particle_DF_analysis.py <run_data.star> <stated resolution> <cutoff 0.05 or 0.01>\nERROR: invalid cutoff value - use 0.01 or 0.05')
 
-def readfile_get_DF(file):
+#-----------------------------------------------------------------------#
+def calculate_0501res(DF,val):
     '''
-    read the run_data.star file with the particles - return list of all defoci
+    Using y = 2.2097x-0.1675 for 0.5
+    and
+    y = 1.3885x - 0.0173 for 0.01
+    calculated using Henning Stahberg's CTF spreadsheet with 0.5,1.5,2.0,3.0 um DF 
     '''
-    defoci = []
-    labels,header,data = read_starfile_new(file)
-    for i in data:
-        DF = (float(i[labels['_rlnDefocusU']])+float(i[labels['_rlnDefocusU']]))/2
-        defoci.append(DF)
-    return(defoci)
+    if val == '0.05':
+        return((2.2097*(DF/10000))-0.1675)
+    if val == '0.01':
+        return((1.3885*(DF/10000))-00.0173)
+#-----------------------------------------------------------------------#
 
-# get yer data
-alldefoci = []
-for i in sys.argv[1:-1]:
-    for j in readfile_get_DF(i):
-        alldefoci.append(j)
-
-def calculate_05res(DF):
-    '''
-    Using y=2.2097x-0.1675 calculated using Henning Stahberg's CTF spreadsheet with 1.0,1.5,2.0,3.0 um DF 
-    '''
-    return((2.2097*(DF/10000))-0.1675)
-
-pixelsize = 1.065
+# make the dict of bins
 bincount = {}
 bins = [round(pixelsize+(x*pixelsize*0.5),1) for x in range(2,12)]
 for i in bins:
     bincount[i] = 0
+goodparts = []
 
 # calculate 0.05 envelope for all parts check against the bins
-srcount = 0 
-for i in alldefoci:
-    env = calculate_05res(i)
-    print (i/10000)
-    print(env)
+labels,header,data = read_starfile_new(sys.argv[1])
+for i in data:
+    defocus = (float(i[labels['_rlnDefocusU']])+float(i[labels['_rlnDefocusU']]))/2
+    env = calculate_0501res(defocus,cutoff)
     for j in bins:
         if float(j) > env:
             bincount[j]+=1
     if statedres > env:
-        srcount+=1
+        goodparts.append(i)
+        
 # plot it
 binkeys = list(bincount)
 binkeys.sort()
@@ -80,9 +76,17 @@ plt.bar(binkeys,revbinvals,tick_label=binkeys,width=0.2,align='center')
 plt.xlabel('Resolution')
 plt.ylabel('Effective # particles')
 plt.grid(axis='y')
-plt.plot(statedres,srcount,marker='o',markersize=10,markeredgecolor='k',c='r')
-plt.text(statedres-0.1,srcount,'{0}A\n{1} parts'.format(statedres,srcount))
+plt.plot(statedres,len(goodparts),marker='o',markersize=10,markeredgecolor='k',c='r')
+plt.text(statedres-0.1,len(goodparts),'{0}A\n{1} parts'.format(statedres,len(goodparts)))
 plt.gca().invert_xaxis()
+plt.title('cutoff = {0}'.format(cutoff))
 plt.tight_layout()
 plt.savefig('effective_partno.png')
 plt.show()
+
+# write a starfile with selected particles
+starout = open('CTFe_filt.star','w')
+for i in header:
+    starout.write('{0}\n'.format(i))
+for i in goodparts:
+    starout.write('{0}\n'.format('    '.join(i)))
